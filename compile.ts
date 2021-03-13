3ed0df41d0df978ba7866e250d883a79a1a4c283
Ian C. McCormack <mccormic2812@uwec.edu>
2021-03-13 17:44:32 -0600

    Fixed file detection for chapter and cover templates.

--- compile.ts
+++ compile.ts
@@ -19,23 +19,42 @@ const args = yargs
         alias: "o",
         type: "string"
     })
-    .option("chapter-template", {
-        alias: "ch",
+    .option("template", {
+        alias: "temp",
         type: "string"
-    })    
-    .option("cover-template", {
-        alias: "cv",
-        type: "string"
-    })    
+    })  
     .argv;
 
 const sourceDir = args.input ? args.input : "./";
-
-const defaultTemplate = !args['chapter-template'] || !args['cover-template'] ? fs.readFileSync("./template.html") : null;
-let chapterTemplate = args['chapter-template'] ? fs.readFileSync(args['chapter-template']) : defaultTemplate;
-let coverTemplate = args['cover-template'] ? fs.readFileSync(args['cover-template']) : defaultTemplate;
+if(!fs.existsSync(sourceDir)){
+    out.msg("The specified source directory '" + sourceDir + "' does not exist.");
+    process.exit(1);
+}
 
 const destinationDir = args.output ? args.output : "./";
+if(!fs.existsSync(destinationDir)){
+    out.msg("The specified destination directory '" + destinationDir + "' does not exist.");
+    process.exit(1);
+}
+
+const templateDir = args.template ? args.template : "./";
+if(!fs.existsSync(templateDir)){
+    out.msg("The specified template directory '" + templateDir + "' does not exist.");
+    process.exit(1);
+}
+
+if(!fs.existsSync(path.join(templateDir, "chapter.html"))){
+    out.msg("The specified template directory '" + templateDir + "' does not contain a 'chapter.html' template.");
+    process.exit(1);
+}
+if(!fs.existsSync(path.join(templateDir, "cover.html"))){
+    out.msg("The specified template directory '" + templateDir + "' does not contain a 'cover.html' template.");
+    process.exit(1);
+}
+
+let chapterTemplate = fs.readFileSync(path.join(templateDir, "chapter.html"));
+let coverTemplate = fs.readFileSync(path.join(templateDir, "cover.html"));
+
 
 const CONTENT_TARGET_ID = "content";
 const TABLE_TARGET_ID = "table";
@@ -51,10 +70,6 @@ if(unfilteredSourceFilenames.length == 0){
 const validSourceFilenames = generator.filterSourceFiles(sourceDir, unfilteredSourceFilenames);
 let renderedChapters = generator.generateChapters(validSourceFilenames);
 
-renderedChapters.forEach(() => {
-    
-})
-
 let chapterDOM = cheerio.load(chapterTemplate, { decodeEntities: true });
 let coverDOM = cheerio.load(coverTemplate, { decodeEntities: true });
 
@@ -68,4 +83,12 @@ let coverTableDiv = coverDOM("#" + TABLE_TARGET_ID);
 
 if(!fs.existsSync(destinationDir)){
     fs.mkdirSync(destinationDir);
+}
+
+for(let chapterIndex = 0; chapterIndex < renderedChapters.length; ++chapterIndex){
+    let chapter = renderedChapters[chapterIndex];
+    chapterContentDiv.html(chapter.content);
+    chapterTableDiv.html(chapter.table);
+    let htmlRendered = chapterDOM.html({ decodeEntities: true });
+    fs.writeFileSync(path.join(destinationDir, "ch" + chapterIndex + ".html"), htmlRendered);
 }
